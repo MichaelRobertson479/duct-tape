8896fdda8fcc9ff762cfa675327cd0d18336aa51
Michael Robertson <mrobertson00@stuy.edu>
2019-11-09 14:55:32 -0500

    e

---
+++ devrandom.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcnt1.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int random() {
+
+    int file = open("/dev/random",O_RDONLY);
+
+    int random;
+    read("/dev/random",random,sizeof(int));
+
+    close("/dev/random");
+
+    return random;
+}
+
+
+int main() {
+
+    printf("random integer: %d",random());
+
+    return 0;
+}
